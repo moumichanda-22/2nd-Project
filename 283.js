d3c0c50732eba8728f4c09ba6cf083f35090ac8f
Mumi Chanda <moumi.chanda21@gmail.com>
2023-07-31 11:33:55 -0400

    added a leetcode example

---
+++ 283.js
@@ -0,0 +1,14 @@
+nums = [0,1,0,3,12]
+var moveZeroes = function(nums) {
+
+let non_zero = [];
+let zero = [];
+while(zero<nums.length){
+    if (nums[zero] == 0) {
+        zero++;
+    
+}
+
+}
+
+}
